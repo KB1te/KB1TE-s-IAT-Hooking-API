097b1600aaa3103aa0561d4997b52485c80cab4b
KB1te <63610437+KB1te@users.noreply.github.com>
2021-01-09 12:06:50 -0300

    Update hook.cpp

--- hook.cpp
+++ hook.cpp
@@ -44,11 +44,32 @@ BOOL Hook::HOOK(LPCSTR toHook, LPVOID myFunction, LPCSTR modName, LPVOID hookedA
 
 BOOL Hook::UnHook(Hook MyHook)
 {
-	MEMORY_BASIC_INFORMATION memInfo;
-	ZeroMemory(&memInfo, sizeof(memInfo));
-	VirtualQuery(memInfo.BaseAddress, &memInfo, sizeof(MEMORY_BASIC_INFORMATION));
-	VirtualProtect(memInfo.BaseAddress, memInfo.RegionSize, PAGE_READWRITE, &memInfo.Protect);
-	this->origFunc = this->origFunc;
-	VirtualProtect(memInfo.BaseAddress, memInfo.RegionSize, memInfo.Protect, &memInfo.Protect);
+	while (pDesc->Name) {
+		toRead = (PIMAGE_THUNK_DATA)(&modInfo.lpBaseOfDll + pDesc->OriginalFirstThunk);
+		toChange = (PIMAGE_THUNK_DATA)(&modInfo.lpBaseOfDll + pDesc->FirstThunk);
+		if (_strcmpi(modName, (char *)pDesc->Name)) {
+			while (toRead->u1.Function) {
+				if (toRead->u1.Ordinal & IMAGE_ORDINAL_FLAG) {
+					PIMAGE_IMPORT_BY_NAME pName = (PIMAGE_IMPORT_BY_NAME)(&modInfo.lpBaseOfDll + toRead->u1.AddressOfData);
+					if (_strcmpi(toHook, pName->Name)) {
+						MEMORY_BASIC_INFORMATION memInfo;
+						VirtualQuery(memInfo.BaseAddress, &memInfo, sizeof(MEMORY_BASIC_INFORMATION));
+						VirtualProtect(memInfo.BaseAddress, memInfo.RegionSize, PAGE_READWRITE, &memInfo.Protect);
+						toChange->u1.Function = this->origFunc;
+						VirtualProtect(memInfo.BaseAddress, memInfo.RegionSize, memInfo.Protect, &memInfo.Protect);
+						this->hookedAddress = &toChange->u1.Function;
+						this->modName = (char *)pDesc->Name;
+						return TRUE;
+					}
+					else {
+						pName++;
+					}
+				}
+				toRead++;
+				toChange++;
+			}
+		}
+		pDesc++;
+	}
 	return TRUE;
 }
