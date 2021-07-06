84ed1432cc8650e2194ac70f9cd2a4419975689f
Σrw1n <63610437+KB1te@users.noreply.github.com>
2021-07-06 16:59:22 -0300

    Update hook.cpp

--- hook.cpp
+++ hook.cpp
@@ -1,48 +1,24 @@
-#include "hook.h"
+#include <Windows.h>
+#include <Psapi.h>
 
-
-BOOL Hook::HOOK(LPCSTR toHook, LPVOID myFunction)
-{
-	this->origFunc = GetProcAddress(GetModuleHandleA(modName), toHook);
-	DWORD				oldProc	= NULL;
-	HMODULE				hModule = GetModuleHandleA(NULL);
-	MODULEINFO			ModInfo = { NULL };
-	PIMAGE_DOS_HEADER		pDos    = { NULL };
-	PIMAGE_NT_HEADERS		pNt     = { NULL };
-	PIMAGE_IMPORT_DESCRIPTOR	pDesc   = { NULL };
-	PIMAGE_THUNK_DATA		change  = { NULL };
-	PIMAGE_THUNK_DATA		read    = { NULL };
-	PIMAGE_IMPORT_BY_NAME		pName	= { NULL };
-
-	GetModuleInformation(GetCurrentProcess(), hModule, &ModInfo, sizeof(ModInfo)); 
-	pDos = (PIMAGE_DOS_HEADER)(ModInfo.lpBaseOfDll); 
-	pNt = (PIMAGE_NT_HEADERS)((DWORD_PTR)ModInfo.lpBaseOfDll + pDos->e_lfanew);
-	pDesc = (PIMAGE_IMPORT_DESCRIPTOR)((DWORD_PTR)ModInfo.lpBaseOfDll + pNt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress);
-
-	while(pDesc->OriginalFirstThunk) {
-		read = (PIMAGE_THUNK_DATA)((DWORD_PTR)ModInfo.lpBaseOfDll + pDesc->OriginalFirstThunk);
-		change = (PIMAGE_THUNK_DATA)((DWORD_PTR)ModInfo.lpBaseOfDll + pDesc->FirstThunk);
-		while(read->u1.Function) {
-			pName = (PIMAGE_IMPORT_BY_NAME)((DWORD_PTR)ModInfo.lpBaseOfDll + read->u1.AddressOfData);
-			if (_strcmpi(pName->Name,toHook)) {
-				VirtualProtect((LPVOID)change->u1.Function, sizeof(LPVOID), PAGE_EXECUTE_READWRITE, &oldProc);
-				change->u1.Function = myFunction;
+int main(void) {
+	HMODULE	hModule = GetModuleHandleA("user32.dll");
+	MODULEINFO ModInfo = { NULL };
+	GetModuleInformation(GetCurrentProcess(), hModule, &ModInfo, sizeof(ModInfo));
+	PIMAGE_NT_HEADERS pNt = (PIMAGE_NT_HEADERS)((int *)ModInfo.lpBaseOfDll + 0x3C);
+	PIMAGE_IMPORT_DESCRIPTOR pDesc = (PIMAGE_IMPORT_DESCRIPTOR)((int *)ModInfo.lpBaseOfDll + 0x168);
+	for (; pDesc->OriginalFirstThunk; pDesc++) {
+		PIMAGE_THUNK_DATA read = (PIMAGE_THUNK_DATA)((int *)ModInfo.lpBaseOfDll + pDesc->OriginalFirstThunk);
+		PIMAGE_THUNK_DATA change = (PIMAGE_THUNK_DATA)((int *)ModInfo.lpBaseOfDll + pDesc->FirstThunk);
+		for (; read->u1.Function; read++, change++) {
+			PIMAGE_IMPORT_BY_NAME pName = (PIMAGE_IMPORT_BY_NAME)((int *)ModInfo.lpBaseOfDll + read->u1.AddressOfData);
+			if (_strcmpi(pName->Name, "MessageBoxA")) {
+				int oldProc = 0;
+				VirtualProtect((LPVOID)change->u1.Function, sizeof(LPVOID), PAGE_EXECUTE_READWRITE, (PDWORD)&oldProc);
+				change->u1.Function = 0;
 				VirtualProtect((LPVOID)change->u1.Function, sizeof(LPVOID), oldProc, NULL);
 			}
-			read++, change++;
 		}
-		pDesc++;
 	}
-	return FALSE;
-}
-
-
-BOOL Hook::UnHook(Hook MyHook)
-{
-	DWORD old = NULL;
-	VirtualProtect(this->hookedAddress, sizeof(LPVOID), PAGE_EXECUTE_READWRITE, &old);
-	this->hookedAddress = this->origFunc;
-	VirtualProtect(this->hookedAddress, sizeof(LPVOID), old, NULL);
-	return TRUE;
-					
+	return 0;
 }
